dc6a73a293fc73216489d1f6b4a84724c34beec0
Klint <klintwales@gmail.com>
2020-02-02 19:04:07 -0600

    Created landing page Added Items activity and intent

--- app/src/main/java/com/example/inventory/MainActivity.java
+++ app/src/main/java/com/example/inventory/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.inventory;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnItems, btnLocations, btnAddItem, btnAddLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnItems = findViewById(R.id.btnItems);
+        btnLocations = findViewById(R.id.btnLocations);
+        btnAddItem = findViewById(R.id.btnAddItem);
+        btnAddLocation = findViewById(R.id.btnAddLocation);
+
+        btnItems.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openItems();
+            }
+        });
+
     }
+
+    public void openItems(){
+        Intent intent = new Intent(this, Items.class);
+        startActivity(intent);
+    }
+
 }
