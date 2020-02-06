eae8e23f49439c79fbebbe6c5038dd23e31f244c
Klint <klintwales@gmail.com>
2020-02-05 20:55:58 -0600

    Added code to allow an item to be added to db

--- app/src/main/java/com/example/inventory/AddItem.java
+++ app/src/main/java/com/example/inventory/AddItem.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 public class AddItem extends AppCompatActivity {
     EditText edtAddItemDescription, edtAddItemLocation, edtAddItemQuantity;
@@ -21,6 +27,25 @@ public class AddItem extends AppCompatActivity {
         edtAddItemQuantity = findViewById(R.id.edtAddItemQuantity);
         btnAddItem = findViewById(R.id.btnAddItem);
 
+        btnAddItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final ParseObject items = new ParseObject("Items");
+                items.put("description", edtAddItemDescription.getText().toString());
+                items.put("location", edtAddItemLocation.getText().toString());
+                items.put("quantity", edtAddItemQuantity.getText().toString());
+                items.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e == null){
+                        Toast.makeText(AddItem.this, items.get("description") + " was added to inventory",Toast.LENGTH_SHORT).show();
+                    }
+                        else{
+                            Toast.makeText(AddItem.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }});
+            }
+        });
 
     }
 }
