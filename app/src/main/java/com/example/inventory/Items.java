ac5290327e2a52afafdb3f5b626b758426e5ccb4
Klint <klintwales@gmail.com>
2020-02-06 21:53:41 -0600

    working on setting up a recycler view

--- app/src/main/java/com/example/inventory/Items.java
+++ app/src/main/java/com/example/inventory/Items.java
@@ -1,8 +1,12 @@
 package com.example.inventory;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
@@ -10,18 +14,36 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Items extends AppCompatActivity {
 
-    private String allItems;
+    private String allDescriptions;
+    private String allLocations;
+    private String allQuantities;
+
+
+    private RecyclerView rvItems;
+
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerAdapter adapter;
+    private ArrayList<String> descriptions = new ArrayList<>();
+    private ArrayList<String> locations = new ArrayList<>();
+    private ArrayList<String> quantity = new ArrayList<>();
+
+    private EditText edtItemLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
 
-        allItems = "";
+        edtItemLocation = findViewById(R.id.edtItemLocation);
+
+
+
+        /*allItems = "";
 
         ParseQuery<ParseObject> queryAll = ParseQuery.getQuery("Items");
         queryAll.findInBackground(new FindCallback<ParseObject>() {
@@ -46,7 +68,46 @@ public class Items extends AppCompatActivity {
                     Toast.makeText(Items.this, e.getMessage() + "", Toast.LENGTH_SHORT).show();
                 }
             }
+
+
+        });*/
+
+
+
+        ParseQuery<ParseObject> queryAll = ParseQuery.getQuery("Items");
+        queryAll.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e == null){
+                    for (ParseObject items : objects){
+                            allDescriptions = items.get("description") + "";
+                            allLocations = items.get("location") + "";
+                            allQuantities = items.get("quantity") + "";
+
+                            for (int i = 0; i < objects.size(); i++){
+                            descriptions.add(allDescriptions);
+                            locations.add(allLocations);
+                            quantity.add(allQuantities);
+                        }
+                    }
+                }
+            }
+
+
+
         });
 
+        initRecyclerView();
+
     }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.rvItems);
+        RecyclerAdapter adapter = new RecyclerAdapter(descriptions, locations, quantity, this  );
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/inventory/RecyclerAdapter.java
@@ -0,0 +1,68 @@
+package com.example.inventory;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder>{
+
+    private ArrayList<String> mDescription = new ArrayList<>();
+    private ArrayList<String> mLocation = new ArrayList<>();
+    private ArrayList<String> mQuantity = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerAdapter(ArrayList<String> mDescription, ArrayList<String> mLocation, ArrayList<String> mQuantity, Context mContext) {
+        this.mDescription = mDescription;
+        this.mLocation = mLocation;
+        this.mQuantity = mQuantity;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.items_layout, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        holder.description.setText(mDescription.get(position));
+        holder.location.setText(mLocation.get(position));
+        holder.quantity.setText(mQuantity.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDescription.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView description;
+        TextView location;
+        TextView quantity;
+        RelativeLayout parentLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            description = itemView.findViewById(R.id.tvItemDescription);
+            location = itemView.findViewById(R.id.tvItemLocation);
+            quantity = itemView.findViewById(R.id.tvItemQuantity);
+
+        }
+    }
+
+}
